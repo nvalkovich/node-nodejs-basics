753ecad825fb0480ee4ac26e069546c2e5822b1f
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-27 01:49:45 +0300

    feat: implement fs rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { rename as fsRename } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+    const fileDir = 'files'
+    const oldFileName = 'wrongFilename.txt';
+    const newFileName = 'properFilename.md';
+
+    const pathToOldFile = path.join(__dirname, fileDir, oldFileName);
+    const pathToNewFile = path.join(__dirname, fileDir, newFileName);
+
+    try {
+        await fsRename(pathToOldFile, pathToNewFile);
+    } catch {
+        throw new Error ('FS operation failed');
+    } 
 };
 
 await rename();
