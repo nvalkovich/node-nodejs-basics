e78c19d04f949f22deca8f0d2dd33945dc261f0a
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-27 02:02:01 +0300

    feat: implement fs list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+    const filesDir = 'files';
+    const pathToFilesDir = path.join(__dirname, filesDir);
+    
+    try {
+        const files = await readdir(pathToFilesDir);
+        console.log(files);
+    } catch {
+        throw new Error ('FS operation failed')
+    }
 };
 
 await list();
