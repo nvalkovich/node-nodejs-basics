17f2601d051b84dfb4cee21bbd620ac800e5fa80
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-26 23:43:25 +0300

    feat: implement fs create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
-};
+    const fileName = 'fresh.txt';
+    const fileDir = 'files';
+    const data = 'I am fresh and young';
+
+    const pathToFile = path.join(__dirname, fileDir, fileName);
+
+    try { 
+        await writeFile(pathToFile, data, { flag: 'wx' });
+    } catch (err) {
+        throw new Error ('FS operation failed');
+    }
+}
+
 
 await create();
