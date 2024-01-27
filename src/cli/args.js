6d1c10ee1d82dee5a9349c6f79a902beca2c74a9
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-27 03:07:26 +0300

    feat: implement cli parseArgs func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const prefix = '--'
+    const result = [];
+
+    process.argv.forEach((value, index) => {
+        if (value.startsWith(prefix)) {
+            result.push(`${value.slice(prefix.length)} is ${process.argv[index + 1]}`)
+        }
+    });
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
