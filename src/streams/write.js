e9e1469301c26033d75d4bf34175bce9236b6553
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-29 01:31:15 +0300

    feat: implement streams write func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from 'node:fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const fileDir = 'files'
+    const fileName = 'fileToWrite.txt';
+    const pathToFile = path.join(__dirname, fileDir, fileName);
+
+    const writableStream = createWriteStream(pathToFile)
+    process.stdin.pipe(writableStream);
 };
 
 await write();
