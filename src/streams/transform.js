5137c5055adc61594893344883761f40b9794ceb
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-29 01:46:36 +0300

    feat: implement streams transform func

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, enc, callback) { 
+            const stringifiedChunk = chunk.toString().trim();
+            const reversedChank = stringifiedChunk.split('').reverse().join('');
+            this.push(reversedChank); 
+            callback();
+        }
+    })
+        
+    process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
