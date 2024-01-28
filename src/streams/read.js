db6d09336192fc570b56c81a89b2aa4603b4c929
nvalkovich <anastasiya.valkovich@mail.ru>
2024-01-29 01:04:47 +0300

    feat: implement streams read func

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'node:fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const fileDir = 'files'
+    const fileName = 'fileToRead.txt';
+    const pathToFile = path.join(__dirname, fileDir, fileName);
+
+    const readableStream = createReadStream(pathToFile);
+    readableStream.pipe(process.stdout);
 };
 
 await read();
